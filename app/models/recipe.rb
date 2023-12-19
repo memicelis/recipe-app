21a226bac1ea65be5c43d281bbae45cc6479144f
memicelis <elismemic5@gmail.com>
2023-12-19 11:35:53 +0100

    Create models for User, FoodRecipe, Food and Recipe

---
+++ app/models/recipe.rb
@@ -0,0 +1,9 @@
+class Recipe < ApplicationRecord
+    belongs_to :user
+    has_many :recipe_foods
+    has_many :foods, through :recipe_foods
+
+    validates :name, :preparation_time, :cooking_time, :description, :public presence: true 
+    validates :preparation_time,:cooking_time, numericality: { only_integer: true, greater_than: 0 }
+    validates :public, inclusion: { in: [true, false] }
+end
