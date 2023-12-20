b8a807588ab67d5b62594a42950a4e04748a0e37
memicelis <elismemic5@gmail.com>
2023-12-20 15:16:50 +0100

    Fix bug

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,6 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
 
-  validates :name, :preparation_time, :cooking_time, :description, :public, presence: true
+  validates :name, :preparation_time, :cooking_time, :description, presence: true
   validates :preparation_time, :cooking_time, numericality: { only_integer: true, greater_than: 0 }
-  validates :public, inclusion: { in: [true, false] }
 end
