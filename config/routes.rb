90756dccffa0edc257c36123713db08aaa3adfb6
memicelis <elismemic5@gmail.com>
2023-12-20 15:15:49 +0100

    Add update to recipes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipe_foods/new'
+  get 'recipe_foods/create'
+  get 'recipe_foods/destroy'
   devise_for :users
   root 'public_recipes#index'
 
-  resources :recipes, only: [:index, :show, :create, :destroy] do
+  resources :recipes, only: [:index, :show, :create, :new, :destroy, :update] do
     resources :recipe_foods, except: :update
   end
 
