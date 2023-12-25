b6d78d9457261499e7f1e4dec99f21b4267ae31b
memicelis <elismemic5@gmail.com>
2023-12-25 10:24:37 +0100

    Fix migration issue

--- db/migrate/20231219105910_add_devise_to_users.rb
+++ db/migrate/20231219105910_add_devise_to_users.rb
@@ -3,9 +3,7 @@
 class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
     change_table :users do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      ## Database authenticatabl
 
       ## Recoverable
       t.string   :reset_password_token
